0540955864f7caff59653a4d7021e5a5510bd796
joaoval01 <jjvalduga@gmail.com>
2021-11-24 16:57:12 -0300

    validate image

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,20 @@ class Post < ApplicationRecord
   validates_length_of :name, maximum: 45, minimum: 2
   validates_length_of :summary, maximum: 100, minimum: 5
   validates_length_of :content, maximum: 500, minimum: 20
+  validate :image_type
+
+
+  private
+
+  def image_type
+    if image.attached? == false
+      errors.add(:image, "is missing!")
+    end
+    if image.blob.byte_size > 3000000
+      errors.add(:image, 'is too big bro')
+    end
+    if !image.content_type.in?(%('image/jpeg image/png'))
+      errors.add(:image, "needs to be a jpeg or png!")
+    end
+  end
 end
